73c5396604767d59c3915af99bb7cb0935e8c25f
IssaParty <abduswamadissa@gmail.com>
2023-03-05 19:42:55 -0500

    chosing python over c#

---
+++ usePython.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+public class PythonScript : MonoBehaviour {
+
+    void Start () {
+        var engine = Python.CreateEngine();
+        var scope = engine.CreateScope();
+        var source = engine.CreateScriptSourceFromFile("./");
+        source.Execute(scope);
+    }
+
+}
